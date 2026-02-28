93a935d81f6b26ed8ff71525f926504d8aa433ec
Aztech Pyramid <108695259+AztechPyramid@users.noreply.github.com>
2026-02-28 08:40:43 +0300

    Delete hooks directory

--- hooks/useGlobalSoundEffects.ts
+++
@@ -1,5 +0,0 @@
-// Global sound effects hook - disabled to avoid annoying click sounds
-export const useGlobalSoundEffects = () => {
-  // Click and navigation sounds disabled - users found them intrusive
-  // Individual components can still use useSoundEffects() for specific interactions
-};
